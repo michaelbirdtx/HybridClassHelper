c87335c83cdb133dcb2688f67c8eb3c3eb2ef074
Michael Bird <github@michaelbirdtx.com>
2022-12-23 20:01:51 -0600

    Further Shadow Warrior development.

--- HybridClassHelper.lua
+++ HybridClassHelper.lua
@@ -7,20 +7,6 @@ local BATTLEMAGE_INTRO_TEXT = "Congratulations, you are now a Battlemage!\n\n"..
     "In addition to these abilities, you will gain additional skills as you level up. And level up you will, as your XP will accrue much faster as a Hybrid Class.\n\n"..
     "Now, go forth, and become legend!"
 
-function HybridClassHelper_OnLoad()
-    this:RegisterEvent("CHAT_MSG_ADDON");
-    end
-
-function HybridClassHelper_OnEvent()
-	if (event == "CHAT_MSG_ADDON" and arg1 == "HybridClassHelper") then
-		if (arg2 == "Grant 801") then
-            SetHybridActionBar("BATTLEMAGE")
-        elseif (arg2 == "Grant 401") then
-            SetHybridActionBar("HIGHWAYMAN")
-        end
-	end
-end
-
 local function clearActionBar(from, to)
     for i = from, to do
         PickupAction(i);
@@ -29,49 +15,88 @@ local function clearActionBar(from, to)
     end
 end
 
+local function setActionSlot(spell, location)
+    PickupSpell(spell);
+    PlaceAction(location);
+    ClearCursor();
+end
+
+function HybridClassHelper_OnLoad()
+    this:RegisterEvent("CHAT_MSG_ADDON");
+    end
+
+function HybridClassHelper_OnEvent()
+	if (event == "CHAT_MSG_ADDON" and arg1 == "HybridClassHelper") then
+		if (arg2 == "Grant 801") then
+            clearActionBar(1, 12)
+            SetHybridActionBar("BATTLEMAGE")
+        elseif (arg2 == "Grant 401") then
+            clearActionBar(1, 12)
+            clearActionBar(73, 84)
+            SetHybridActionBar("HIGHWAYMAN")
+        elseif (arg2 == "Grant 101") then
+            clearActionBar(1, 12)
+            clearActionBar(73, 84)
+            clearActionBar(85, 96)
+            clearActionBar(97, 108)
+            SetHybridActionBar("SHADOWWARRIOR")
+        end
+	end
+end
+
 function SetHybridActionBar(hybridClass)
-    clearActionBar(1, 12)
+    
     if (hybridClass == "HIGHWAYMAN") then
-        PickupSpell("Auto Shot");
-        PlaceAction(1);
-        ClearCursor();
-        PickupSpell("Poisoned Shot");
-        PlaceAction(2);
-        ClearCursor();
-        PickupSpell("Sinister Strike");
-        PlaceAction(3);
-        ClearCursor();
-        PickupSpell("Slice and Dice");
-        PlaceAction(4);
-        ClearCursor();
-        PickupSpell("Eviscerate");
-        PlaceAction(5);
-        ClearCursor();
-        PickupSpell("Trueshot Aura");
-        PlaceAction(12);
-        ClearCursor();
+        setActionSlot("Auto Shot", 1);
+        setActionSlot("Poisoned Shot", 2);
+        setActionSlot("Sinister Strike", 3);
+        setActionSlot("Slice and Dice", 4);
+        setActionSlot("Eviscerate", 5);
+        setActionSlot("Trueshot Aura", 11);
+        setActionSlot("Mount Speed+ DND", 12);
     end
     if (hybridClass == "BATTLEMAGE") then
-        PickupSpell("Auto Attack");
-        PlaceAction(1);
-        ClearCursor();
-        PickupSpell("Shoulder Charge");
-        PlaceAction(2);
-        ClearCursor();
-        PickupSpell("Arcane Cleave");
-        PlaceAction(3);
-        ClearCursor();
-        PickupSpell("Fire Blast");
-        PlaceAction(4);
-        ClearCursor();
-        PickupSpell("Frost Nova");
-        PlaceAction(5);
-        ClearCursor();
-        PickupSpell("Arcane Intellect");
-        PlaceAction(11);
-        ClearCursor();
-        PickupSpell("Frost Armor");
-        PlaceAction(12);
-        ClearCursor();
+        setActionSlot("Auto Attack", 1);
+        setActionSlot("Shoulder Charge", 2);
+        setActionSlot("Arcane Cleave", 3);
+        setActionSlot("Fire Blast", 4);
+        setActionSlot("Frost Nova", 5);
+        setActionSlot("Arcane Intellect", 10);
+        setActionSlot("Frost Armor", 11);
+        setActionSlot("Mount Speed+ DND", 12);
+    end
+    if (hybridClass == "SHADOWWARRIOR") then
+        setActionSlot("Auto Attack", 1);
+        setActionSlot("Shadowstep", 2);
+        setActionSlot("Shadowsurge", 3);
+        setActionSlot("Victory Rush", 4);
+        setActionSlot("Thunder Clap", 5);
+        setActionSlot("Resist Shadow", 10);
+        setActionSlot("Shadowform", 11);
+        setActionSlot("Mount Speed+ DND", 12);
+        setActionSlot("Auto Attack", 73);
+        setActionSlot("Shadowstep", 74);
+        setActionSlot("Shadowsurge", 75);
+        setActionSlot("Victory Rush", 76);
+        setActionSlot("Thunder Clap", 77);
+        setActionSlot("Resist Shadow", 82);
+        setActionSlot("Shadowform", 83);
+        setActionSlot("Mount Speed+ DND", 84);
+        setActionSlot("Auto Attack", 85);
+        setActionSlot("Shadowstep", 86);
+        setActionSlot("Shadowsurge", 87);
+        setActionSlot("Victory Rush", 88);
+        setActionSlot("Thunder Clap", 89);
+        setActionSlot("Resist Shadow", 94);
+        setActionSlot("Shadowform", 95);
+        setActionSlot("Mount Speed+ DND", 96);
+        setActionSlot("Auto Attack", 97);
+        setActionSlot("Shadowstep", 98);
+        setActionSlot("Shadowsurge", 99);
+        setActionSlot("Victory Rush", 100);
+        setActionSlot("Thunder Clap", 101);
+        setActionSlot("Shadowform", 106);
+        setActionSlot("Shadowform", 107);
+        setActionSlot("Mount Speed+ DND", 108);
     end
 end

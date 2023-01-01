a76fb0673d001e6ccebdc39447cc738db7b16b4c
Michael Bird <github@michaelbirdtx.com>
2022-12-31 23:00:22 -0600

    Action bar changes.

--- HybridClassHelper.lua
+++ HybridClassHelper.lua
@@ -1,12 +1,3 @@
-local BATTLEMAGE_INTRO_TEXT = "Congratulations, you are now a Battlemage!\n\n"..
-    "Your new Hybrid Class combines all the abilities of a Mage, with several new abilities that make you a formidable\nmelee fighter."..
-    "Your Stamina and Armor have been increased, and you will be able to dual-wield one-handed weapons.\n\n"..
-    "You also start with two new abilities: Shoulder Charge and Arcane Cleave. Shoulder Charge allows you to rush an opponent and stun them with your impact."..
-    "Arcane Cleave allows you to strike multiple opponents with a single blow.\n\n"..
-    "To speed you in your quests, cast Mount Speed+ to enjoy a super-fast riding experience!\n\n"..
-    "In addition to these abilities, you will gain additional skills as you level up. And level up you will, as your XP will accrue much faster as a Hybrid Class.\n\n"..
-    "Now, go forth, and become legend!"
-
 local function clearActionBar(from, to)
     for i = from, to do
         PickupAction(i);
@@ -60,8 +51,9 @@ function SetHybridActionBar(hybridClass)
         setActionSlot("Shoulder Charge", 2);
         setActionSlot("Arcane Cleave", 3);
         setActionSlot("Fire Blast", 4);
-        setActionSlot("Frost Nova", 5);
-        setActionSlot("Arcane Intellect", 10);
+        setActionSlot("Fireball", 5);
+        setActionSlot("Frost Nova", 6);
+        setActionSlot("Arcane Intellect", 10);        
         setActionSlot("Frost Armor", 11);
         setActionSlot("Mount Speed+ DND", 12);
     end
